07ae990283af424ea82bdab483ec2b2995a6cb29
romanku <roma.kulakov@gmail.com>
2018-09-19 11:43:12 +0300

    Update index.js
    master modification

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 const hotfix;
+const modification;
 ReactDOM.render(
   <App />,
   document.getElementById('main')
